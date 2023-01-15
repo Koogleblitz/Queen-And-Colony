504a35e2a1cf2ce4011568c42ecebceeffe7d8e4
Noor Alaghbar <noorga115@gmail.com>
2023-01-15 00:21:22 -0800

    Fixed warning spam when spawned creatures move
    Stops calling animation changes when "hasAnimations" is set to false in CreatureController2D

--- Scripts/CreatureController2D.cs
+++ Scripts/CreatureController2D.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class CreatureController2D : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
+    // movement speed
     [SerializeField] float speed = 2f;
+
+    // prevents errors when moving if no animations
+    [SerializeField] bool hasAnimations = false;
+
+    Rigidbody2D rigidbody2d;
     Vector2 motionVector;
     public Vector2 lastMotionVector;
     Animator animator;
@@ -15,7 +21,8 @@ public class CreatureController2D : MonoBehaviour
     void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        if (hasAnimations)
+            animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -29,18 +36,25 @@ public class CreatureController2D : MonoBehaviour
             horizontal,
             vertical
             );
-        animator.SetFloat("Horizontal", horizontal);
-        animator.SetFloat("Vertical", vertical);
 
-        moving = horizontal != 0 || vertical != 0;
-        animator.SetBool("moving", moving);
+        if (hasAnimations)
+        {
+            animator.SetFloat("Horizontal", horizontal);
+            animator.SetFloat("Vertical", vertical);
+
+            moving = horizontal != 0 || vertical != 0;
+            animator.SetBool("moving", moving);
+        }
 
         if (horizontal != 0 || vertical != 0)
         {
             lastMotionVector = new Vector2(horizontal, vertical).normalized;
 
-            animator.SetFloat("lastHorizontal", horizontal);
-            animator.SetFloat("lastVertical", vertical);
+            if (hasAnimations)
+            {
+                animator.SetFloat("lastHorizontal", horizontal);
+                animator.SetFloat("lastVertical", vertical);
+            }
         }
     }
 
