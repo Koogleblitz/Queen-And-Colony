a5d5fe857918beb9048e7d53116169bb93f0d6ac
songbai <2ndbsmith@gmail.com>
2023-01-15 05:41:13 -0800

    push new

---
+++ Scripts/HpandAttack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpandAttack : MonoBehaviour
+{
+
+    public int HitPoint = 100;
+    public int attackPower = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(close to enemy)        
+    }
+
+    void OnCombact()
+    {
+        // minus hp according to enemy's attackpower
+
+    }
+
+}
