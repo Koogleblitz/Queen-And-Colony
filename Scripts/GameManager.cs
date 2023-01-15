75e8f07bf8b5ebadbe58f8bfab8ca85a66217d52
songbai <2ndbsmith@gmail.com>
2023-01-15 04:15:36 -0800

    fix bug on eggs

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -10,11 +10,14 @@ public class GameManager : MonoBehaviour
     public int[] foodRequirement = {10,20,50,100};
     public int[] timeSpawn       = {3,5,10,20};
     // TODO If we have time 
+
+    public int currentfood = 100;
+    public int maxfood = 500;
+
     // Greater ants needs more food and time spawn
 
-    public List<Rigidbody2D> enemyList           = new List<Rigidbody2D>();
-    public List<Rigidbody2D> discoveredEnemyList = new List<Rigidbody2D>();
-
+    public List<Transform> enemyList           = new List<Transform>();
+    public List<Transform> discoveredEnemyList = new List<Transform>();
 
     // number of frames between wave spawns
     [SerializeField] int framesUntilWave = 10 * 30; //10 sec @ 30fps 
