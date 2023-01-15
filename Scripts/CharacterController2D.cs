b25dd7329e3e81bedd504a94645df462e346bddb
Richard Tobing <richardtobing@outlook.com>
2023-01-14 21:00:08 -0800

    added first assets and scripts

---
+++ Scripts/CharacterController2D.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    [SerializeField] float speed= 2f;
+    Vector2 motionVector;
+    Animator animator;
+
+
+
+
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();   
+        animator = GetComponent<Animator>();
+    }
+
+    // I think tis gets 
+    private void Update()
+    {
+        motionVector = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        );
+
+        animator.SetFloat("horizontal", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        rigidbody2d.velocity = motionVector * speed;
+    }
+}
